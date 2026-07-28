7ed1ebd71d29fc4f54df6c934227848310b14435
dlxogns03 <ikingdragon@naver.com>
2026-07-28 17:40:31 +0900

    day9 interface

--- oop/src/com/ktdsuniversity/edu/oop/vendingmachine/CoffeeVendingMachineExam3.java
+++ oop/src/com/ktdsuniversity/edu/oop/vendingmachine/CoffeeVendingMachineExam3.java
@@ -1,5 +1,8 @@
 package com.ktdsuniversity.edu.oop.vendingmachine;
 
 public interface CoffeeVendingMachineExam3 {
-	
+	void choiceCoffee(String name);
+	void addextrashot(int addnumber, int price);
+	void choicepayment(String name);
+	void buying(int amount, int cash);
 }
--- oop/src/com/ktdsuniversity/edu/oop/vendingmachine/RamenVendingMachineExam2.java
+++ oop/src/com/ktdsuniversity/edu/oop/vendingmachine/RamenVendingMachineExam2.java
@@ -1,5 +1,9 @@
 package com.ktdsuniversity.edu.oop.vendingmachine;
 
 public interface RamenVendingMachineExam2 {
+	
+	void choiceRamen(String name);
+	void choiceAddgarnish(String name, int price);
+	void buy(int Amount, int cash);
 
 }
